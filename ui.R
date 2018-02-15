7cf9da5b1056cab5f0c54a733eb4b220e945f226
Andrew Sila <a.sila@cgiar.org>
2018-02-15 09:13:34 +0300

    Removed spaces

--- ui.R
+++ ui.R
@@ -12,7 +12,9 @@ library(reshape2)
 library(ggplot2)
 library(dplyr)
 library(plotly)
+
 library(xlsx)
+
 library(shinyjs)
 
 appCSS <- "#loading-content {  position: absolute;  background: #000000;  opacity: 0.9;  z-index: 100;  left: 0;  right: 0;  height: 100%; text-align: center; color: #FFFFFF;}"
